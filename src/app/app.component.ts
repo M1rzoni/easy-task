354b00ee1e8f51898fd81dae3fbd0644248c0699
Mirza <mirza.sabanovic@ipi-akademija.ba>
2024-09-18 15:31:25 +0200

    Displaying tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+
+  selectedUsersId = 'u1';
+
+  get selectedUsers() {
+    return this.users.find((user) => user.id === this.selectedUsersId)!;
+  }
+
+  onSelectedUser(id: string) {
+    this.selectedUsersId = id;
+  }
 }
